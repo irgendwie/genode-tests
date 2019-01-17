8c64e21b175b2e4a82629f715f64d54738edc750
Alexander Weidinger <alexander.weidinger@tum.de>
2019-01-17 15:38:25 +0100

    Remove ip address and header

--- src/app/child/main.cc
+++ src/app/child/main.cc
@@ -1,17 +1,3 @@
-/*
- * \brief  Test client for the Hello RPC interface
- * \author Björn Döbel
- * \author Norman Feske
- * \date   2008-03-20
- */
-
-/*
- * Copyright (C) 2008-2017 Genode Labs GmbH
- *
- * This file is part of the Genode OS framework, which is distributed
- * under the terms of the GNU Affero General Public License version 3.
- */
-
 #include <base/component.h>
 #include <base/log.h>
 #include <libc/component.h>
@@ -121,7 +107,7 @@ void Libc::Component::construct(Libc::Env &env)
 		    struct sockaddr_in srv_addr;
 		    srv_addr.sin_port = htons(8080);
 		    srv_addr.sin_family = AF_INET;
-		    srv_addr.sin_addr.s_addr = inet_addr("131.159.12.17");
+		    srv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
 
 		    int sd = ::socket(AF_INET, SOCK_STREAM, 0);
 		    ::connect(sd, (struct sockaddr *)&srv_addr, sizeof(srv_addr));
